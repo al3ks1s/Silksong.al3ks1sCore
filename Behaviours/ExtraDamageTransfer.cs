b4abeb6a10217ed576fced79e2cd674430ed2494
al3ks1s <al3ks1sss@gmail.com>
2026-09-18 11:52:50 +0200

    Transfer Damager Behaviours

---
+++ Behaviours/ExtraDamageTransfer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace al3ks1sCore.Behaviours
+{
+    internal class ExtraDamageTransfer : MonoBehaviour
+    {
+
+        public GameObject Target;
+
+        private HealthManager _selfHealth;
+        private HealthManager _targetHealth;
+
+        public void Start()
+        {
+
+            _selfHealth = GetComponent<HealthManager>();
+            _targetHealth = Target.GetComponent<HealthManager>();
+
+            if (_selfHealth != null && _targetHealth != null)
+                _selfHealth.TookDamage += TransferDamage;
+
+        }
+
+        public void TransferDamage()
+        {
+            if (_targetHealth.hp - _selfHealth.lastHitInstance.DamageDealt > 0)
+                _targetHealth.ApplyExtraDamage(_selfHealth.lastHitInstance.DamageDealt);
+        }
+
+
+    }
+}
